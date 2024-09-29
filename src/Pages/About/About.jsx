5951f1d446309577cfe5c315abe7dcf1fa2f97f4
abusaiyedjoy <abusaiyedjoy1@gmail.com>
2024-09-30 02:56:03 +0600

    Modified Contact & About Section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,16 +1,173 @@
 import Cover from "../../Layout/Cover/Cover";
+import Header from "../Header/Header";
 import AboutFeatures from "./AboutFeatures";
 
 const About = () => {
 
     return (
-        <div className={`min-h-screen`}>
+        <div className="min-h-screen bg-[#F5F2EB] dark:bg-gray-800">
             <Cover
                 image={'https://i.ibb.co/bstNQ2D/bri-tucker-LGG5-P7-KCzi-U-unsplash.jpg'}
                 header={"About Us"}
                 title={" / About Us"}
-                />
+            />
             <AboutFeatures></AboutFeatures>
+            {/* Frequently Asked Questions */}
+            <Header Heading={"FAQs (Frequently Asked Questions)"} SubHeading={"More About Us"}></Header>
+            <div className="space-y-4 max-w-4xl mx-auto pb-16">
+                <details
+                    className="group border-s-4 border-third bg-gray-50 p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+                    open
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900 dark:text-white">
+                        1. How can I adopt a pet?
+                        </h2>
+
+                        <span
+                            className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3 dark:bg-gray-800 dark:text-white"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700 dark:text-gray-200">
+                    To adopt a pet, simply browse our available pets on the "Pet Listing" page, select the one you'd like to adopt, and fill out the adoption application form. Our team will review your application and get in touch to guide you through the next steps.
+                    </p>
+                </details>
+
+                <details
+                    className="group border-s-4 border-third bg-gray-50 p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900 dark:text-white">
+                        2. What is the adoption process like?
+                        </h2>
+
+                        <span
+                            className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3 dark:bg-gray-800 dark:text-white"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700 dark:text-gray-200">
+                    Our adoption process includes filling out an application, a screening process, and a meet-and-greet with the pet. Once approved, you'll complete the necessary paperwork and take your new companion home!
+                    </p>
+                </details>
+                <details
+                    className="group border-s-4 border-third bg-gray-50 p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900 dark:text-white">
+                        3. What should I prepare before adopting a pet?
+                        </h2>
+
+                        <span
+                            className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3 dark:bg-gray-800 dark:text-white"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700 dark:text-gray-200">
+                    Before adopting, ensure you have the proper supplies, like food, water bowls, bedding, toys, and a safe, comfortable space for your new pet. Also, be prepared for the time and commitment it takes to care for an animal.
+                    </p>
+                </details>
+                <details
+                    className="group border-s-4 border-third bg-gray-50 p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900 dark:text-white">
+                        4. Can I meet the pet before adopting?
+                        </h2>
+
+                        <span
+                            className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3 dark:bg-gray-800 dark:text-white"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700 dark:text-gray-200">
+                    Yes, we encourage prospective adopters to meet the pet in person or through a virtual meet-and-greet. You can schedule a meeting after submitting your application.
+                    </p>
+                </details>
+                <details
+                    className="group border-s-4 border-third bg-gray-50 p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+                >
+                    <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                        <h2 className="text-lg font-medium text-gray-900 dark:text-white">
+                        5. What if the pet doesn’t adjust well to my home?
+                        </h2>
+
+                        <span
+                            className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3 dark:bg-gray-800 dark:text-white"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </span>
+                    </summary>
+
+                    <p className="mt-4 leading-relaxed text-gray-700 dark:text-gray-200">
+                    We understand that not all adoptions are a perfect fit. If the pet doesn't adjust well to your home, we will work with you to find a solution, which may include returning the pet to us.
+                    </p>
+                </details>
+            </div>
         </div>
     );
 };
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,14 +1,35 @@
+/* eslint-disable react/no-unescaped-entities */
 import Cover from "../../Layout/Cover/Cover";
 import { BsFillAirplaneEnginesFill } from "react-icons/bs";
+import { FaPhone } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoIosMailUnread } from "react-icons/io"
 
 const Contact = () => {
     return (
-        <section className="bg-[#FFFFFF] dark:bg-gray-900">
+        <section className="bg-[#d3d3d3] dark:bg-gray-900">
             <Cover
                 image={'https://i.ibb.co/bstNQ2D/bri-tucker-LGG5-P7-KCzi-U-unsplash.jpg'}
                 header={"Contact Us"}
                 title={" / Contact Us"}
             />
+            <div className="flex flex-wrap gap-16 my-16">
+                <div className="bg-[#F5F2EB] relative dark:bg-gray-700 pb-6 pt-12 px-10 text-center max-w-lg mx-auto rounded-xl">
+                    <div className="absolute -top-8 w-16 h-16 left-1/2 transform -translate-x-1/2 rounded-full bg-third hover:scale-125 duration-200 ease-in-out flex justify-center items-center cursor-pointer text-white"><IoIosMailUnread size={32} /></div>
+                    <h1 className="text-2xl font-bold sofadi pb-4 text-gray-800 dark:text-gray-200 ">Write for US</h1>
+                    <a href="mailto:abusaiyedjoy1@gmail.com" className="hover:text-secondary text-gray-800 dark:text-gray-200 dark:hover:text-secondary">abusaiyedjoy1@gmail.com</a>
+                </div>
+                <div className="bg-[#F5F2EB] relative dark:bg-gray-700 pb-6 pt-12 px-10 text-center max-w-lg mx-auto rounded-xl">
+                    <div className="absolute -top-8 w-16 h-16 left-1/2 transform -translate-x-1/2 rounded-full bg-third hover:scale-125 duration-200 ease-in-out flex justify-center items-center cursor-pointer text-white"><FaLocationDot size={32} /></div>
+                    <h1 className="text-2xl font-bold sofadi pb-4  text-gray-800 dark:text-gray-200">Visit our Office</h1>
+                    <p className=" text-gray-800 dark:text-gray-200">Foy's Lake / Chittagong <br /> Bangladesh</p>
+                </div>
+                <div className="bg-[#F5F2EB] relative dark:bg-gray-700 pb-6 pt-12 px-10 text-center max-w-lg mx-auto rounded-xl">
+                    <div className="absolute -top-8 w-16 h-16 left-1/2 transform -translate-x-1/2 rounded-full bg-third hover:scale-125 duration-200 ease-in-out flex justify-center items-center cursor-pointer text-white"><FaPhone size={32}/></div>
+                    <h1 className="text-2xl font-bold sofadi pb-4  text-gray-800 dark:text-gray-200">Contact with US</h1>
+                    <p className=" text-gray-800 dark:text-gray-200">+8801823567254</p>
+                </div>
+            </div>
             <div className="max-w-2xl w-auto mx-8 mt-8">
                 <h1 className="text-2xl text-primary font-bold sofia">Contact Us</h1>
                 <h2 className="text-gray-800 dark:text-gray-300 text-4xl font-bold roboto ">Send us a message<span className="text-secondary text-5xl">.</span></h2>
@@ -17,42 +38,42 @@ const Contact = () => {
             <div
                 className={`xl:mt-12 flex xl:flex-row flex-col-reverse max-w-4xl mx-auto gap-10 overflow-hidden`}
             >
-                    <form
-                        className='mt-12 mx-8 flex flex-col gap-8 '
-                    >
-                        <label className='flex flex-col'>
-                            <span className='dark:text-gray-200 text-gray-500 font-medium mb-4 sofadi'>👨‍✈️ Your Name</span>
-                            <input
-                                type='text'
-                                name='name'
-                                placeholder="What's your good name?"
-                                className='dark:bg-gray-700 bg-[#F5F2EB] py-4 px-6 placeholder:text-gray-400 text-white rounded-lg outline-none border-none font-medium'
-                            />
-                        </label>
-                        <label className='flex flex-col'>
-                            <span className='dark:text-gray-200 text-gray-500 font-medium mb-4 sofadi'>📩 Your email</span>
-                            <input
-                                type='email'
-                                name='email'
-                                placeholder="What's your web address?"
-                                className='dark:bg-gray-700 bg-[#F5F2EB] py-4 px-6 placeholder:text-gray-400 text-white rounded-lg outline-none border-none font-medium'
-                            />
-                        </label>
-                        <label className='flex flex-col'>
-                            <span className='dark:text-gray-200 text-gray-500 font-medium mb-4 sofadi'>📝 Your Message</span>
-                            <textarea
-                                rows={7}
-                                name='message'
-                                placeholder='What you want to say?'
-                                className='dark:bg-gray-700 bg-[#F5F2EB] py-4 px-6 placeholder:text-gray-400 text-white rounded-lg outline-none border-none font-medium'
-                            />
-                        </label>
-                        <button type='submit' className="dark:bg-gray-700 w-52 bg-[#F5F2EB] py-2 px-2 rounded-xl outline-none font-bold h-16 mb-4 mt-2 dark:hover:bg-gray-800 duration-300 text-gray-600 dark:text-gray-200 flex items-center justify-center group">
-                            <BsFillAirplaneEnginesFill className="icon rotate-45 group-hover:duration-700 absolute w-12 -translate-x-full translate-y-full scale-0 group-hover:scale-100 group-hover:translate-x-8 group-hover:-translate-y-8 duration-150" />
+                <form
+                    className='my-12 mx-8 flex flex-col gap-8 '
+                >
+                    <label className='flex flex-col'>
+                        <span className='dark:text-gray-200 text-gray-800 font-medium mb-4 sofadi'>👨‍✈️ Your Name</span>
+                        <input
+                            type='text'
+                            name='name'
+                            placeholder="What's your good name?"
+                            className='dark:bg-gray-700 bg-[#F5F2EB] py-4 px-6 placeholder:text-gray-400 tegr8 dark:text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+                    <label className='flex flex-col'>
+                        <span className='dark:text-gray-200 text-gray-800 font-medium mb-4 sofadi'>📩 Your email</span>
+                        <input
+                            type='email'
+                            name='email'
+                            placeholder="What's your web address?"
+                            className='dark:bg-gray-700 bg-[#F5F2EB] py-4 px-6 placeholder:text-gray-400 tegr8 dark:text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+                    <label className='flex flex-col'>
+                        <span className='dark:text-gray-200 text-gray-800 font-medium mb-4 sofadi'>📝 Your Message</span>
+                        <textarea
+                            rows={7}
+                            name='message'
+                            placeholder='What you want to say?'
+                            className='dark:bg-gray-700 bg-[#F5F2EB] py-4 px-6 placeholder:text-gray-400 text-gray-800 dark:text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+                    <button type='submit' className="dark:bg-gray-700 w-52 bg-[#F5F2EB] py-2 px-2 rounded-xl outline-none font-bold h-16 mb-4 mt-2 dark:hover:bg-gray-800 duration-300 text-gray-600 dark:text-gray-200 flex items-center justify-center group">
+                        <BsFillAirplaneEnginesFill className="icon rotate-45 group-hover:duration-700 absolute w-12 -translate-x-full translate-y-full scale-0 group-hover:scale-100 group-hover:translate-x-8 group-hover:-translate-y-8 duration-150" />
 
-                            <span className="duration-500 text-xl">Send Message</span></button>
+                        <span className="duration-500 text-xl">Send Message</span></button>
 
-                    </form>
+                </form>
             </div>
         </section>
     );
